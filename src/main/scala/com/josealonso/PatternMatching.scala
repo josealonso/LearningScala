ffe8f13098165ae68cbe7afede4be64c461279a9
josealonso <jose.alonso.programmer@gmail.com>
2023-09-09 20:50:03 +0200

    Using pattern matching to deconstruct or decompose different data structures.

--- src/main/scala/com/josealonso/PatternMatching.scala
+++ src/main/scala/com/josealonso/PatternMatching.scala
@@ -9,4 +9,29 @@ object PatternMatching extends App {
     case _ => anInteger + "th"
   }
   // Pattern Matching is an expression
+
+  // Case class decomposition
+  case class Person(name: String, age:Int)
+  val alice = Person("Alice", 25)
+
+  val personGreeting = alice match {
+    case Person(n, a) => s"Hi, my name is $n and I am $a years old."
+    case _ => "something else"
+  }
+  println(personGreeting)
+
+  // deconstructing tuples
+  val aTuple = ("Queen", "Rock")
+  val bandDescription =aTuple match {
+    case (band, genre) => s"$band belongs to the genre $genre."
+    case _ => "unknown band"
+  }
+
+  // decomposing lists
+  val aList = List(1, 2, 3)
+  val listDescription = aList match {
+    case List(_, 2, _) => "List containing 2 on its second position"
+    case _ => "unknown list"
+  }
+
 }
