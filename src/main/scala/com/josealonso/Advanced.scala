89444f4e0555434f58a526f7667ceb3faa7de864
josealonso <jose.alonso.programmer@gmail.com>
2023-09-10 00:21:25 +0200

    The Option type returns either a Some value. It avoids NPE errors.

--- src/main/scala/com/josealonso/Advanced.scala
+++ src/main/scala/com/josealonso/Advanced.scala
@@ -11,5 +11,15 @@ object Advanced extends App {
   val eagerValue = lazyValueWithSideEffect + 1  // Nothing is printed if this line is commented
   // useful in infinite collections
 
+  // pseudo-collections: Option, Try
+  def methodWhichCanReturnNull() = "Hello, Scala"
+  val anOption = Option(methodWhichCanReturnNull())  // Some(value) or None
+  // option = "collection" which contains at most one element: Some(value) or None
+
+  val stringProcessing = anOption match {
+    case Some(string) => s"I have obtained a valid string: $string"
+    case None => "I obtained nothing"
+  }
+  // map, flatMap, filter
 
 }
