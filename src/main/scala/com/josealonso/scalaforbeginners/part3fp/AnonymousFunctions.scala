aa97ac3693c14eac9d4acb2b853e79f2615f2fb0
josealonso <jose.alonso.programmer@gmail.com>
2023-09-21 19:46:35 +0200

    Syntax for anonymous functions.

---
+++ src/main/scala/com/josealonso/scalaforbeginners/part3fp/AnonymousFunctions.scala
@@ -0,0 +1,27 @@
+package com.josealonso.scalaforbeginners.part3fp
+
+object AnonymousFunctions extends App {
+
+  val doubler: Int => Int = x => x * 2
+
+  // multiple params
+  val adder: (Int, Int) => Int = (x, y) => x + y
+
+  // no params
+  val doSomething: () => Int = () => 3
+
+  // be careful with the parentheses
+  println(doSomething)    // function itself
+  println(doSomething())  // call
+
+  // curly braces with lambdas
+  val sringToInt = { (str: String) =>
+    str.toInt
+  }
+
+  // more syntactic sugar
+  val adder2: (Int, Int) => Int = _ + _  // useful notation in HOF calls
+q
+  val superAdd = (x: Int) => (y: Int) => x + y
+  println(superAdd(3)(4))
+}
