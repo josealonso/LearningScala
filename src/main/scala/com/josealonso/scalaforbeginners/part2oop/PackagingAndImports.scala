51cee019792a034a7ecbfffad10efb804369476b
josealonso <jose.alonso.programmer@gmail.com>
2023-09-21 14:17:55 +0200

    Packages in Scala.

---
+++ src/main/scala/com/josealonso/scalaforbeginners/part2oop/PackagingAndImports.scala
@@ -0,0 +1,10 @@
+package com.josealonso.scalaforbeginners.part2oop
+
+object PackagingAndImports {
+
+  // Packages mirror the file structure, like in Java
+
+  // "package objects" hold standalone methods and constants (one per package)
+  // They are specific to Scala
+
+}
