ff3f7a270bfe387c1d00f4da07a910334f5b5277
josealonso <jose.alonso.programmer@gmail.com>
2023-09-26 03:13:15 +0200

    Hierarchy for immutable collections.

---
+++ src/main/scala/com/josealonso/scalaforbeginners/part3fp/ImmutableCollections.scala
@@ -0,0 +1,22 @@
+package com.josealonso.scalaforbeginners.part3fp
+
+object ImmutableCollections {
+
+  /*
+                                      Traversable
+                                           |
+                        Set ----------> Iterable <--------- Map
+                                           |
+                        IndexedSeq -----> Seq <------ LinearSeq
+   */
+
+  /*
+    class HashSet extends Set
+    class SortedSet extends Set
+    class HashMap extends Map
+    class SortedMap extends Map
+
+    Children of IndexedSeq: Vector, String, Range.
+    Children of LinearSeq: List, Stream, Stack, Queue.
+   */
+}
